9763ebe7763802089d24414a16a8c39833b147c5
Yogesh Nikam <yogesh.nikam@thoughtworks.com>
2021-11-13 12:58:53 +0530

    Transaction that demonstrates the All or Nothing (Atomicity) behaviour of any transaction

--- src/main/java/kafka/producer/FirstProducer.java
+++ src/main/java/kafka/producer/FirstProducer.java
@@ -29,11 +29,17 @@ public class FirstProducer {
         logger.info("The very important step before beginning any transaction, takes care of many things that are required for a successful transaction");
         producer.initTransactions();
 
-        logger.info("Beginning the transaction...");
-        producer.beginTransaction();
+        ALLTransaction(producer);
+        NOTHINGTransaction(producer);
 
+        producer.close();
+    }
+
+    public static void ALLTransaction(KafkaProducer<Integer, String> producer) {
+        logger.info("Beginning the ALL transaction...");
+        producer.beginTransaction();
         try {
-            logger.info("Sending the messages in transaction...");
+            logger.info("Sending the messages in ALL transaction...");
             producer.send(new ProducerRecord<>(KafkaConfig.topicName1, 0, "Hi Aditya!"));
             producer.send(new ProducerRecord<>(KafkaConfig.topicName2, 1, "I am Yogesh :)"));
 
@@ -46,7 +52,25 @@ public class FirstProducer {
             producer.close();
             throw new RuntimeException("Error during transaction and hence no messages are sent to the topics (ATOMICITY)");
         }
+    }
 
-        producer.close();
+    public static void NOTHINGTransaction(KafkaProducer<Integer, String> producer) {
+        logger.info("Beginning the NOTHING transaction...");
+        producer.beginTransaction();
+        try {
+            logger.info("Sending the messages in NOTHING transaction...");
+            producer.send(new ProducerRecord<>(KafkaConfig.topicName1, 0, "Hello Nisha :)"));
+            producer.send(new ProducerRecord<>(KafkaConfig.topicName2, 1, "Hola Friends!"));
+
+            logger.info("Aborting the messages in transaction to demonstrate the All or Nothing (ATOMICITY) behaviour of a transaction...");
+            logger.info("In simple words, none of the above messages should be sent in case we abort the transaction");
+            producer.abortTransaction();
+        }
+        catch(Exception e) {
+            logger.info("Aborting the transaction because of error during transaction...");
+            producer.abortTransaction();
+            producer.close();
+            throw new RuntimeException("Error during transaction and hence no messages are sent to the topics (ATOMICITY)");
+        }
     }
 }
